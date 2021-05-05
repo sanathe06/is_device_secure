27577bfb424f78fc2989e4e25b098b6f71b54a58
Alex Melnyk <ask.melnyk@gmail.com>
2021-05-06 01:27:02 +0300

    Dart 2.12 migrated with Null-Safety.

--- lib/is_device_secure.dart
+++ lib/is_device_secure.dart
@@ -5,13 +5,13 @@ import 'package:flutter/services.dart';
 class IsDeviceSecure {
   static const MethodChannel _channel = const MethodChannel('is_device_secure');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<bool> get secure async {
-    final bool isSecurred = await _channel.invokeMethod("isSecure");
-    return isSecurred;
+  static Future<bool?> get secure async {
+    final bool? isSecured = await _channel.invokeMethod("isSecure");
+    return isSecured;
   }
 }
